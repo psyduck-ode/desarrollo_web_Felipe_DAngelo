80b8c5bf5cbd50380a100e89cd287f4011dc20de
psyduck-ode <feidangel@hotmail.com>
2025-09-29 22:46:24 -0300

    changed the conection to tarea2.sql

--- database/tarea2.sql
+++ database/tarea2.sql
@@ -1,4 +1,4 @@
--- Active: 1759191830955@@127.0.0.1@3306@mysql
+-- Active: 1759196501809@@localhost@3306@tarea2
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
